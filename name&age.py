bb59e0ac82ae38dab945dfb35addae0fd9cb278e
jose-gomes1 <josemgomes042017@gmail.com>
2025-09-16 10:39:27 +0000

    Condições

--- name&age.py
+++ name&age.py
@@ -9,13 +9,13 @@ respostas_true = ["sim","true","correto","verdade","1"]
 respostas_false = ["não","nao","incorreto","falso", "false","0"]
 
 print(f"O teu nome é", name, "e tens", idade, "anos")
-booleano = input("És maior de idade?\n").lower()
-if booleano in respostas_true:
+respostaUser = input("És maior de idade?\n").lower()
+if respostaUser in respostas_true:
     if idade >= 18:
         print("És maior de idade")
     else:
         print("Estás a mentir")
-elif booleano in respostas_false:
+elif respostaUser in respostas_false:
     if idade < 18:
         print("És menor de idade")
     else:
