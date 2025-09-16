b9b92a6a104f1a32ad32dcdb68e28d005851ed09
jose-gomes1 <josemgomes042017@gmail.com>
2025-09-16 10:30:46 +0000

    Condições

--- name&age.py
+++ name&age.py
@@ -3,5 +3,22 @@ from datetime import date
 name = input("Como te chamas?\n")
 ano = input("Qual o teu ano de nascimento?\n")
 hoje = date.today()
+idade = hoje.year - int(ano)
 
-print(f"O teu nome é", name, "e tens", hoje.year - int(ano), "anos")
+respostas_true = ["sim","true","correto","verdade","1"]
+respostas_false = ["não","nao","incorreto","falso", "false","0"]
+
+print(f"O teu nome é", name, "e tens", idade, "anos")
+booleano = input("És maior de idade?\n").lower()
+if booleano in respostas_true:
+    if idade >= 18:
+        print("És maior de idade")
+    else:
+        print("Estás a mentir")
+elif booleano in respostas_false:
+    if idade - int(ano) < 18:
+        print("És menor de idade")
+    else:
+        print("Estás a mentir")
+else:
+    print("Resposta inválida")
