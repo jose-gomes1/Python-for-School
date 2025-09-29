d61bca89d33a7053fa7d3f5cb91ae5638a7c9a0c
jose-gomes1 <josemgomes042017@gmail.com>
2025-09-29 16:30:21 +0000

    bingoGenerator

---
+++ modulo1/bingo.py
@@ -0,0 +1,17 @@
+import random
+
+random.seed()
+
+a = 0
+bingoNums = list(range(0, 101))
+
+while a != "q":
+    if not bingoNums:
+        print("Acabaram os números")
+        break
+
+    chosen = random.choice(bingoNums)
+    bingoNums.remove(chosen)
+    print("Número:",chosen)
+
+    a = input("Prima qualquer tecla para continuar ('q' para sair)").lower()
