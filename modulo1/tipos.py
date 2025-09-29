e0df9efab1a58abb6d05583d28bfa6a2920a82d7
jose-gomes1 <josemgomes042017@gmail.com>
2025-09-29 14:30:16 +0000

    tipos

--- modulo1/tipos.py
+++ modulo1/tipos.py
@@ -1,12 +1,21 @@
 # listas -> arrays []
 # dictionary -> par chave -> valor {like:json}
 # sets -> mutáveis {}
-# tupples -> imutáveis ()
+# tuplet -> imutáveis ()
 
 # funções lambda
 # funções anónimas de baixa complexidade
 
-# exercício:
+# exercício 1 (5m)
+"""
+Programa onde é definido um tuplet com 4 valores e uma função que aceita como parâmetro um tuplet e que imprime no ecrã o seu conteúdo
 """
 
-"""
+meuTuplet = ("val1", "val2", "val3", "val4")
+
+def funcTuplet(tuplet):
+    for i in tuplet:
+        b = ", ".join(tuplet)
+    print(b)
+    
+funcTuplet(meuTuplet)
