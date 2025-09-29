daca65f2e29b92b2394ffed246776aa9cf02cc2c
jose-gomes1 <josemgomes042017@gmail.com>
2025-09-29 14:55:09 +0000

    tipos

--- modulo1/tipos.py
+++ modulo1/tipos.py
@@ -18,4 +18,36 @@ def funcTuplet(tuplet):
         b = ", ".join(tuplet)
     print(b)
     
-funcTuplet(meuTuplet)
+# funcTuplet(meuTuplet)
+
+# exercício 2
+"""
+Programa um programa que tenha uma função que aceita uma lista de números inteiros e devolve a soma dos seus elementos
+"""
+
+inteiros = [0, 1, 2, 3, 4]
+
+def funcInts(lista):
+    sum = 0
+    for i in lista:
+        sum += lista[i]
+    return sum
+
+# print(funcInts(inteiros))
+
+# exercício 3
+"""
+Programa com uma função que recebe um dicionário e imprime no ecrã no seguinte formato:
+chave: valor
+"""
+
+myDictionary = {"chave1":"valor1",
+                "chave2":"valor2",
+                "chave3":"valor3",
+                "chave4":"valor4"}
+
+def funcDictionary(dict):
+    for key in dict:
+        print(f"{key}:",dict[key])
+    
+funcDictionary(myDictionary)
