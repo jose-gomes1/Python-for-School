5c201a3c6d497a1d2b0d6131441028965c454590
jose-gomes1 <145759337+jose-gomes1@users.noreply.github.com>
2025-09-29 19:29:13 +0000

    semiBingo

--- modulo1/bingoNumberGenerator.py
+++ modulo1/bingoNumberGenerator.py
@@ -2,10 +2,10 @@ import random
 
 random.seed()
 
-a = 0
 bingoNums = list(range(1, 101))
 
 def generatorFunction():
+    a = 0
     while a != "q":
         if not bingoNums:
             print("Acabaram os números")
@@ -16,17 +16,37 @@ def generatorFunction():
         bingoNums.remove(chosen)
         print("Número:",chosen)
 
-        a = input("Prima qualquer tecla para continuar ('q' para sair)").lower()
+        a = input("Prima qualquer tecla para continuar ('q' para sair) ").lower()
 
 def generateCard():
-    numCards = random.sample((1, 101), 8)
-    card = [numCards[0], numCards[1], numCards[2],
-            numCards[3], "Free Space", numCards[4],
-            numCards[5], numCards[6], numCards[7]]
+    numCards = random.sample(range(1, 101), 8)
+    card = [[numCards[0], numCards[1], numCards[2]],
+            [numCards[3], "X", numCards[4]],
+            [numCards[5], numCards[6], numCards[7]]]
+    
     return card
 
 def player():
-    b = input(int("Quantos cartões quer gerar? "))
-    while i in b:
-        card[i] = generateCard()
+    b = int(input("Quantos cartões quer gerar? "))
+    for i in range(b):
+        cartao = generateCard()
+        print(f"Cartão {i+1}:")
+        for row in cartao:
+            for item in row:
+                print(item, end=" ")
+            print()
+
+def start():
+    style = int(input("É jogador (1) ou gerador (2)? "))
+    match style:
+        case 1:
+            player()
+        case 2:
+            generatorFunction()
+        case _:
+            print("Caractér inválido")
+            return
         
+
+if __name__ == "__main__":
+    start()
