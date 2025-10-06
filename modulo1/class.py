e2a2b5cf21e2144232490780e058768e1a4d9378
jose-gomes1 <josemgomes042017@gmail.com>
2025-10-06 15:15:08 +0000

    Classes

--- modulo1/class.py
+++ modulo1/class.py
@@ -1,3 +1,5 @@
+import os
+
 class Person:
     def __init__(self, name, age):
         self.name = name
@@ -6,14 +8,23 @@ class Person:
 p1 = Person("John", 36)
 
 class Carro:
-    def __init__(self, marca, modelo):
+    def __init__(self, marca, modelo, km):
         self.marca = marca
         self.modelo = modelo
+        self.km = km
     def mostraMarca(self):
         print(self.marca, end=" ")
     def mostraModelo(self):
         print(self.modelo)
+    def conduz(self):
+        a = input("Aperte Enter para continuar ou 'n' para parar")
+        while a != "n":
+            print(f"{self.km} km andados")
+            self.km += 1
+            if self.km % 10 == 0:
+                os.system("clear")
+            a = input("Aperte Enter para continuar ou 'n' para parar")
+        print(f"{self.km} km andados")
     
-c1 = Carro("Fiat", "500")
-c1.mostraMarca()
-c1.mostraModelo()
+c1 = Carro("Fiat", "500", 0)
+c1.conduz()
