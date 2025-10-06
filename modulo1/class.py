195603eb23d4f905b61f1aa9c2210bd29ea7e7cf
jose-gomes1 <josemgomes042017@gmail.com>
2025-10-06 15:20:31 +0000

    Classes

--- modulo1/class.py
+++ modulo1/class.py
@@ -8,6 +8,11 @@ class Person:
 p1 = Person("John", 36)
 
 class Carro:
+
+    acelera = 0
+    velocidade = 0
+    velocidadeMax = 255
+
     def __init__(self, marca, modelo, km):
         self.marca = marca
         self.modelo = modelo
@@ -25,6 +30,12 @@ class Carro:
                 os.system("clear")
             a = input("Aperte Enter para continuar ou 'n' para parar")
         print(f"{self.km} km andados")
+    def acelerar(self, acelera):
+        self.velocidade += acelera
+        if self.velocidade >= self.velocidadeMax:
+            self.velocidade = self.velocidadeMax
+        print(f"Acelerando a {self.velocidade} km/h")
+
     
 c1 = Carro("Fiat", "500", 0)
-c1.conduz()
+c1.acelerar(150)
